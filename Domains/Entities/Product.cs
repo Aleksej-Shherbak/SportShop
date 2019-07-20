0c3aa7490804395440804116a24b89049b156bef
aleksej <aleksej.shherbak@yandex.ru>
2019-07-20 23:09:25 +0300

    Added messages about wrong values to UI for edit product functional.

--- Domains/Entities/Product.cs
+++ Domains/Entities/Product.cs
@@ -10,13 +10,18 @@ namespace Domains.Entities
         [ScaffoldColumn(false)]
         public int Id { get; set; }
         
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
         
+        [Required(ErrorMessage = "Please enter a description of the product")]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
-        
+
+        [Required(ErrorMessage = "Please enter a price")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a correct price value")]
         public decimal Price { get; set; }
         
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
