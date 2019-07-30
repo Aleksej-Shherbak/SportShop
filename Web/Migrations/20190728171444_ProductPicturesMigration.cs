006ac3a1d27b548e1e0d864714b9d17eee25bdc2
aleksej <aleksej.shherbak@yandex.ru>
2019-07-30 20:45:04 +0300

    Added images migrations

---
+++ Web/Migrations/20190728171444_ProductPicturesMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domains.Migrations
+{
+    public partial class ProductPicturesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+        }
+    }
+}
